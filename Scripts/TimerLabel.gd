bcfc3057b6071317980310f1e6eb8d5063acd478
Aare Undo <aare@carto.com>
2018-08-11 13:31:33 +0300

    TimerLabel, TotalLabel; scripts

---
+++ Scripts/TimerLabel.gd
@@ -0,0 +1,32 @@
+extends Label
+
+var isPaused = false
+
+var time_total_minutes = 4
+var time_total_seconds = time_total_minutes * 60
+
+var time_start = 0
+var time_now = 0
+
+func _ready():
+	time_start = OS.get_unix_time()
+	pass
+
+func _process(delta):
+	
+	if isPaused:
+		return
+	
+	time_now = OS.get_unix_time()
+	var elapsed = time_now - time_start
+	var elapsed_minutes = elapsed / 60
+	var elapsed_seconds = elapsed % 60
+
+	if elapsed_seconds == 0:
+		return
+	
+	var minutes_left = time_total_minutes - elapsed_minutes
+	var seconds_left = 60 - elapsed_seconds
+	text = "%02d:%02d" % [minutes_left, seconds_left]
+	
+	pass
---
+++ Scripts/TotalLabel.gd
@@ -0,0 +1,13 @@
+extends Label
+
+var score = 500
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _process(delta):
+	
+	text = "Score: " + str(score)
+	pass
