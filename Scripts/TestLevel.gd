bfbfabd4134ad1c512fd9cbfaf8d06b3d0866a9b
Tiit Pikma <thsnr@users.noreply.github.com>
2018-08-11 19:29:21 +0300

    TestLevel: Remove ad-hoc Timer, use the one in FlotsamManager

--- Scripts/TestLevel.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-var img = load("res://icon.png")
-
-func _on_Timer_timeout():
-	$FlotsamManager.spawn_flotsam(Vector2(get_viewport().get_visible_rect().size.x, 300), 3, 10, img, img)
