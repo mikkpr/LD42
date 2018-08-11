4113f6c3eac260144ddee73ec5d522d853fb8e73
Tiit Pikma <thsnr@users.noreply.github.com>
2018-08-11 15:03:23 +0300

    Container: Set background texture through exported variable

--- Scripts/Container.gd
+++ Scripts/Container.gd
@@ -1,6 +1,11 @@
 extends Node
 
+export (Texture) var background  # Texture to use as Container background.
+
 var flotsam  # Flotsam stored in this container.
+
+func _ready():
+	$Background.texture = background
 
 func store(flotsam):
 	self.flotsam = flotsam
