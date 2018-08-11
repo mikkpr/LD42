35f9fda91d404dbc28fbe042065fbfcd2402a77f
Tiit Pikma <thsnr@users.noreply.github.com>
2018-08-11 15:04:19 +0300

    Container: Add Containers which cannot store Flotsam

--- Scripts/Container.gd
+++ Scripts/Container.gd
@@ -1,6 +1,7 @@
 extends Node
 
 export (Texture) var background  # Texture to use as Container background.
+export (bool) var store_flotsam = true  # Can this Container store flotsam?
 
 var flotsam  # Flotsam stored in this container.
 
@@ -8,9 +9,12 @@ func _ready():
 	$Background.texture = background
 
 func store(flotsam):
+	if store_flotsam || flotsam != null:
+		return false
 	self.flotsam = flotsam
 	$Contents.texture = flotsam.squashed_texture
 	$Contents.visible = true
+	return true
 
 func clear():
 	flotsam = null
