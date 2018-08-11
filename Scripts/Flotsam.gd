33e3418703f3ea763fe38ad3d6cf48b5cc641ddd
Stepan Bolotnikov <stepan.bolotnikov@guardtime.com>
2018-08-11 17:49:57 +0300

    Refactored FlotsamManager, Flotsams now float when spawned and die when they reach certain point

--- Scripts/Flotsam.gd
+++ Scripts/Flotsam.gd
@@ -7,6 +7,7 @@ export(Texture) var squashed_texture
 
 var weight_size_coefficient = 10
 var fall_speed = 500
+var float_speed = 200
 
 enum STATES {
 	floating,
@@ -33,6 +34,15 @@ func paint_initial_texture():
 	get_node("Sprite").texture = float_texture
 
 func _process(delta):
+	if flotsam_state == STATES.floating:
+		if position.x < get_parent().flotsam_destroy_line:
+			flotsam_state = STATES.dropped
+			can_be_dragged = false
+		else:
+			var float_vector = Vector2()
+			float_vector.x = - float_speed * delta
+			float_vector.y = 0
+			translate(float_vector)
 	if flotsam_state == STATES.dropped:
 		if global_transform.origin.y - get_node("Sprite").texture.get_size().y/2 > get_viewport().get_visible_rect().size.y:
 			print("Flotsam destroyed")
--- Scripts/FlotsamManager.gd
+++ Scripts/FlotsamManager.gd
@@ -1,12 +1,15 @@
 extends Node
 
+export var flotsam_destroy_line = 300
+
 var flotsam = load("res://Scenes/Flotsam.tscn")
-var img = load("res://icon.png")
+#var img = load("res://icon.png")
 
 func _ready():
-	spawn_flotsam(Vector2(300, 300))
+	#spawn_flotsam(Vector2(get_viewport().get_visible_rect().size.x, 300), 3, 10, img, img)
+	set_process_input(false)
 
-func _input(ev):
+func _input(ev): # made for debug purposes
 	if ev is InputEventMouseButton and ev.button_index == BUTTON_LEFT and ev.pressed:
 		var clicked_on_child = false
 		var local_mouse_coords = get_global_transform().xform_inv(ev.position)
@@ -16,12 +19,14 @@ func _input(ev):
 			if child_rect.has_point(local_mouse_coords):
 				clicked_on_child = true
 		if clicked_on_child == false:
-			spawn_flotsam(get_viewport().get_mouse_position(), true)
+			spawn_flotsam(get_viewport().get_mouse_position(), 3, 10, img, img, true)
 
-func spawn_flotsam(position, dragged = false):
+func spawn_flotsam(position, weight, score, grab_tex, squash_tex, dragged = false):
 	var new_flotsam = flotsam.instance()
-	new_flotsam.init(3, 10, img, img)
+	new_flotsam.init(weight, score, grab_tex, squash_tex)
 	add_child(new_flotsam)
 	new_flotsam.global_position = position
+	# spawn an already dragged Flotsam to mouse position when left button is down or idk what will happen
 	if dragged:
 		new_flotsam.start_drag()
+
