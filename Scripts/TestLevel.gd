84742c9ce744bd128c163a1ec9061f75b85239bc
Tiit Pikma <thsnr@users.noreply.github.com>
2018-08-12 22:07:23 +0300

    TestLevel: Stop timer and show scoreboard on game over

--- Scripts/TestLevel.gd
+++ Scripts/TestLevel.gd
@@ -7,34 +7,25 @@ onready var button = $Scoreboard/UI/MarginContainer/EditContainer/Button
 var isFinished = false
 
 func _ready():
-
 	button.connect("pressed", self, "button_pressed")
 
-	pass
-
 func _process(delta):
-
 	if isFinished:
 		return
 
 	var seconds_left = int(timer.time_left)
-
 	if seconds_left == 0:
 		_initialize_end_screen()
-
-	pass
 
 func _initialize_end_screen():
 	print("The game is finished, showing end screen")
 	isFinished = true
 	scoreboard.show()
 
-
-
 func button_pressed():
 	print("button_pressed")
 	var score = $UIContainer/UI/TotalLabel.get_score()
 	scoreboard.postScore(score)
 
-
-
+func _on_Boat_sunk():
+	_initialize_end_screen() # TODO: show failure screen
--- Scripts/UI.gd
+++ Scripts/UI.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _on_Boat_score(score):
 	$TotalLabel.update(score)
+
+func _on_Boat_sunk():
+	# Use paused, because stop() sets the timer to 00:00.
+	$GameTimer.paused = true
