0d9a0ac298257d99bd62e947e21cba95e986dc3f
Tiit Pikma <thsnr@users.noreply.github.com>
2018-08-12 11:32:13 +0300

    BoatV2: Add rotation mechanic and sunk signal

--- Scripts/BoatV2.gd
+++ Scripts/BoatV2.gd
@@ -1,8 +1,19 @@
-extends Node
+extends Node2D
+
+signal sunk # Emitted when the boat has sunk.
+
+export (float) var degrees_per_tilt = 1.0 # How many degrees to tilt per weight.
+export (int) var degrees_to_sink = 90     # At how many degrees to sink.
 
 var tilt = 0.0    # Weighted sum of the Flotsam weights.
 var score = 0     # Sum of Flotsam scores.
 var active = null # Current active Container.
+
+func _process(delta):
+	# TODO: Add better smoothing.
+	rotation_degrees += (tilt * degrees_per_tilt - rotation_degrees) / 2 * delta
+	if abs(rotation_degrees) > degrees_to_sink:
+		emit_signal("sunk")
 
 func store(flotsam):
 	if active == null || !active.store(flotsam):
