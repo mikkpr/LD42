abb6fefa3a20fe256fc0ff55ba3c4ce5cc2ad962
Mikk <mikk@ikald.us>
2018-08-13 20:14:53 +0300

    Change main menu background and reposition buttons

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -4,16 +4,16 @@ onready var GameScene
 
 
 func _on_TutorialButton_pressed():
-	get_tree().change_scene("res://Scenes/Calculator.tscn")
+    get_tree().change_scene("res://Scenes/Calculator.tscn")
 
 
 func _on_NewGameButton_pressed():
-	get_tree().change_scene("res://Scenes/TestLevel.tscn")
+    get_tree().change_scene("res://Scenes/TestLevel.tscn")
 
 
 func _on_QuitButton_pressed():
-	get_tree().quit()
+    get_tree().quit()
 
 
 func _on_HighScoresButton_pressed():
-	get_tree().change_scene("res://Scenes/HighScores.tscn")
+    get_tree().change_scene("res://Scenes/HighScores.tscn")
--- Scripts/UI.gd
+++ Scripts/UI.gd
@@ -1,10 +1,10 @@
 extends Control
 
 func _on_Boat_score(score):
-	$TotalLabel.update(score)
+    $TotalLabel.update(score)
 
 func _on_Boat_sunk():
-	# Use paused, because stop() sets the timer to 00:00.
-	$GuidanceLabel.randomTip()
-	$GuidanceLabel.show()
-	$GameTimer.paused = true
+    # Use paused, because stop() sets the timer to 00:00.
+    $GuidanceLabel.randomTip()
+    $GuidanceLabel.show()
+    $GameTimer.paused = true
