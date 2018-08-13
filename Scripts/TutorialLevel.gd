35fb628ec18cc3beaa8d138e8a7912212e676b7c
Stepan Bolotnikov <stepan.bolotnikov@guardtime.com>
2018-08-13 21:45:50 +0300

    TutorialLevel: can now play through

--- Scripts/TutorialLevel.gd
+++ Scripts/TutorialLevel.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 onready var retryButton = $Scoreboard/Retry/UI/MarginContainer/ButtonContainer/Button
-onready var menuButton = $Scoreboard/UI/MarginContainer/EditContainer/MenuButton
 
 onready var left_box = $Boat/Cargo.get_child(0)
 onready var right_box = $Boat/Cargo.get_child(1)
@@ -10,7 +9,8 @@ var isFinished = false
 
 func _ready():
 	retryButton.connect("pressed", self, "retry_button_pressed")
-	menuButton.connect("pressed", self, "menu_button_pressed")
+	$Scoreboard/Retry/UI/MarginContainer/Label.text = "Good job! Now on to the real thing!"
+	$Scoreboard/Retry/UI/MarginContainer/ButtonContainer/Button.text = "Continue"
 	$FlotsamManager/Timer.stop()
 	$UIContainer/UI/GameTimer.paused = true
 	$UIContainer/UI/TimerLabel.visible = false
@@ -40,11 +40,13 @@ func tutorial(step):
 			$Boat.connect("rotation", self, "stabilising")
 		6:
 			right_box.visible = true
-			show_tip("Shark weighs less but is farther away so it tilts you more!")
-			spawn_flotsam("shark")
 		7:
 			show_tip("Great job!")
+			$Boat.connect("docked", self, "_on_boat_docked")
 			$Boat.call_deferred("dock")
+
+func _on_boat_docked():
+	$Scoreboard.show(false)
 
 func sinking(amount):
 	if amount > 20:
@@ -56,9 +58,7 @@ func sinking(amount):
 func stabilising(amount):
 	if amount < 10:
 		$Boat.disconnect("rotation", self, "stabilising")
-		tutorial(6)
-	#elif amount < 20:
-	#	tutorial(5)
+		tutorial(5)
 
 func _process(delta):
 	if isFinished:
@@ -68,11 +68,6 @@ func show_tip(message):
 	$UIContainer/UI/GuidanceLabel.text = message
 	$UIContainer/UI/GuidanceLabel.show()
 
-func _initialize_end_screen():
-	print("Tutorial DONE")
-	$Boat.dock()
-	isFinished = true
-	
 func retry_button_pressed():
 	get_tree().change_scene("res://Scenes/TestLevel.tscn")
 
@@ -92,8 +87,11 @@ func _on_flotsam_destroyed(kind):
 
 func _on_left_stored(flotsam):
 	# Whatever is put in the left container stays there forever.
+	$Boat.disconnect("rotation", self, "sinking")
 	left_box.get_node("Contents").get_child(0).input_pickable = false
-	tutorial(5)
+	show_tip("Shark weighs less but is farther away so it tilts you more!")
+	spawn_flotsam("shark")
+	tutorial(6)
 
 func _on_left_removed(flotsam):
 	pass
