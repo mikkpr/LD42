7e29f871f2729e2d3c9e3aba7d92f59b97b3dcbe
Aare Undo <aareundo@Aares-MacBook-Pro.local>
2018-08-13 18:13:15 +0300

    retry screen background overlay

---
+++ Scripts/Retry.gd
@@ -0,0 +1,13 @@
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+
+	pass
+
+func _process(delta):
+	$UI/MarginContainer/Label.margin_top = 90
+	pass
